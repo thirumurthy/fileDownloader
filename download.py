cca436710c915e8dad2da77c107da99d6c4e74b1
kumaravel <kumar.okm1995@gmail.com>
2019-09-19 08:17:49 +0530

    bug fixed

--- download.py
+++ download.py
@@ -31,14 +31,14 @@ if __name__ =='__main__':
     sys.exit (1)
 
  link = sys.argv[1:][0]
-#link = 'https://nl18.seedr.cc/ff_get/510875226/www.TamilRockers.ws%20-%20X-Men%20Dark%20Phoenix%20(2019)[720p%20-%20BDRip%20-%20Org%20Auds%20[Tamil%20%20Telugu%20%20Hindi]%20-%20AC3%205.1%20-%20HEVC].mkv?st=v_kl_vuJ6OenhFktGNYb8w&e=1568024548'
- if not os.path.exists("files"):
+#link = 'https://nl27.seedr.cc/ff_get/515994809/www.TamilRockers.ws%20-%20Bakrid%20(2019)%20Tamil%20HDRip%20x264%20400MB.mkv?st=mK8oo4-6kipmBM5D9W0hNA&e=1568945595'
+if not os.path.exists("files"):
     os.makedirs("files")
- file_name=os.path.normpath("files/"+unquote(os.path.basename(urlparse(link).path)))
- logger.error("filename :"+file_name)
- print("Filename: "+file_name)
- print("Downloading %s" % file_name)
- try:
+file_name=os.path.abspath("files/"+unquote(os.path.basename(urlparse(link).path)))
+logger.error("filename :"+file_name)
+print("Filename: "+file_name)
+print("Downloading %s" % file_name)
+try:
    response = urlopen(link)
    CHUNK = 16 * 1024
    with open(file_name, 'wb') as f:
@@ -47,5 +47,5 @@ if __name__ =='__main__':
         if not chunk:
             break
         f.write(chunk)
-  except Exception as e:
+except Exception as e:
     logger.error(e)
